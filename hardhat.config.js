f69bf840037e0b3d54d33c3afd026088124981db
nikhilvc1990 <nikhil.chandran@iiitmk.ac.in>
2026-01-04 19:45:36 +0530

    adding new simpler code

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,9 +2,21 @@ require("@nomicfoundation/hardhat-toolbox");
 
 module.exports = {
   solidity: "0.8.28",
+  defaultNetwork: "localhost",
   networks: {
     localhost: {
       url: "http://127.0.0.1:8545"
-    }
+    },
+    hardhat: {
+      accounts: {
+        mnemonic: "slot amount waste sense nothing lawsuit muscle course water mobile candy struggle",
+        count: 10, 
+        accountsBalance: "900000000000000000000000",
+      },
+    },
+    // sepolia : {
+    //   url: "YOUR_SEPOLIA_RPC_URL",
+    //   accounts: ["YOUR_PRIVATE_KEY"]
+    // }
   }
 };
